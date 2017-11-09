185840df5c3d9b8f0ef8a62891f7a6f929ae3ba1
Domas <domas.bitvinskas@me.com>
2017-11-09 14:54:28 +0000

    Apollo setup and generate a signature

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -4,6 +4,26 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+const ipcMain = electron.ipcMain;
+const globalShortcut = electron.globalShortcut;
+const clipboard = electron.clipboard;
+
+const ApolloClient = require('apollo-client');
+const ApolloLinkHttp = require('apollo-link-http');
+const ApolloCacheInmemory = require('apollo-cache-inmemory');
+const fetch = require('node-fetch');
+const gql = require('graphql-tag');
+
+const _ = require('lodash');
+
+const client = new ApolloClient.ApolloClient({
+  link: new ApolloLinkHttp.HttpLink({
+    uri: 'http://localhost:4005/graphql',
+    fetch,
+  }),
+  cache: new ApolloCacheInmemory.InMemoryCache(),
+});
+
 const path = require('path');
 const url = require('url');
 
@@ -12,26 +32,26 @@ const url = require('url');
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
-    // and load the index.html of the app.
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
-    mainWindow.loadURL(startUrl);
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // and load the index.html of the app.
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+  mainWindow.loadURL(startUrl);
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null
+  });
 }
 
 // This method will be called when Electron has finished
@@ -41,20 +61,87 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+      app.quit();
+  }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+const clipboardFormats = [
+  {
+    key: 'text',
+    write: 'writeText',
+    read: 'readText',
+  },
+  {
+    key: 'html',
+    write: 'writeHTML',
+    read: 'readHTML',
+  },
+  {
+    key: 'rtf',
+    write: 'writeRTF',
+    read: 'readRTF',
+  },
+  {
+    key: 'image',
+    write: 'writeImage',
+    read: 'readImage',
+  },
+];
+
+const debugAlert = (what) => {
+  mainWindow.webContents.send('debug-alert', what);
+};
+
+ipcMain.on('connect-signature-key', (e, config) => {
+  const pasteSignature = () => {
+    mainWindow.setProgressBar(0);
+
+    const mutation = gql`
+      mutation generateSignature {
+        generateSignature
+      }
+    `;
+
+    client.mutate({ mutation }).then((result) => {
+      try {
+        mainWindow.setProgressBar(0.5);
+
+        const clipboardBackup = {};
+
+        _.each(clipboardFormats, (format) => {
+          clipboardBackup[format.key] = clipboard[format.read]();
+        });
+
+        clipboard.writeHtml(result.data.generateSignature);
+
+        mainWindow.setProgressBar(1);
+      }
+      catch(e) {
+        debugAlert(e.message + e.stack);
+      }
+      finally {
+        _.each(clipboardFormats, (format) => {
+          clipboard[format.write](clipboardBackup[format.key]);
+        });
+
+        mainWindow.setProgressBar(-1);
+        setTimeout(() => {
+          mainWindow.setProgressBar(-1);
+        }, 1000);
+      }
+    });
+  };
+
+  globalShortcut.register('CmdOrCtrl+G', pasteSignature);
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './App';
-import './index.css';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4005/graphql' }),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
